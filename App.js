9da496fd912576006333f85771d1a43bd37dab48
Damon <damon@usp.br>
2023-11-29 13:00:30 -0300

    add SafeAreaView to all screens

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { StyleSheet, ImageBackground } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 import StartGameScreen from './screens/StartGameScreen';
 import GameScreen from './screens/GameScreen';
@@ -20,7 +21,9 @@ export default function App() {
   return (
     <LinearGradient style={styles.rootScreen} colors={['#4e0329', '#ddb52f']}>
       <ImageBackground style={styles.rootScreen} imageStyle={styles.backgroundImage} source={require('./assets/images/background.png')} resizeMode='cover'>
-        {screen}
+        <SafeAreaView style={styles.rootScreen}>
+          {screen}
+        </SafeAreaView>
       </ImageBackground>
     </LinearGradient>
   );
--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -1,15 +1,27 @@
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet , View, Text } from 'react-native';
 
 function GameScreen() {
     return(
-        <View>
-            <Text>GameScreen</Text>
+        <View style={styles.screen}>
+            <Text>Opponent's Guesses</Text>
+            {/* GUESSES */}
+            <View>
+                <Text>Higher or Lower?</Text>
+                {/* Buttons + and - */}
+            </View>
+            <View>
+                {/* ROUNDS */}
+            </View>
         </View>
     );
 };
 
 const styles = StyleSheet.create({
+    screen: {
+        flex: 1,
+        padding: 12,
 
+    }
 });
 
 export default GameScreen;
